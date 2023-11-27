8e3ff83d27bca9275f653de1eab0881b672d89c7
Nick Qi <nickqi@tubi.tv>
2023-11-27 22:34:07 +0800

    feat: update table config

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,5 @@
 import { Table } from 'antd';
+import dayjs from 'dayjs';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
@@ -6,22 +7,23 @@ import { parseStatusCode } from 'src/utils';
 
 import styles from './Logs.module.scss';
 
+const genColumn = (key, others) => ({
+  key,
+  dataIndex: key,
+  ...others,
+});
+
 const columns = [
-  {
+  genColumn('time', {
     title: 'Time',
-    dataIndex: 'time',
-    key: 'time',
-  },
-  {
+    sorter: (a, b) => dayjs(a.time).diff(dayjs(b.time)),
+  }),
+  genColumn('statusCode', {
     title: 'Status Code',
-    dataIndex: 'statusCode',
-    key: 'statusCode',
-  },
-  {
+  }),
+  genColumn('msg', {
     title: 'Message',
-    dataIndex: 'msg',
-    key: 'msg',
-  },
+  }),
 ];
 
 const Logs = ({ logJsons }) => {
@@ -35,6 +37,7 @@ const Logs = ({ logJsons }) => {
     dataSource: logJsons.map((json, idx) => ({
       ...json,
       key: idx,
+      time: dayjs(json.time).format('YYYY-MM-DD HH:mm:ss'),
       statusCode: parseStatusCode(json),
     })),
     expandable: {
