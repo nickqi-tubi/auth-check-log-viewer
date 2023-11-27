ae63cdce09d7308a121d015c3ec9d078701a5e49
Nick Qi <nickqi@tubi.tv>
2023-11-27 14:32:27 +0800

    feat: parse log file content

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, Upload } from 'antd';
+import { useState } from 'react';
 
 import styles from './App.module.scss';
 
 const App = () => {
+  const [logJsons, setLogJsons] = useState([]);
+
   const uploadProps = {
-    name: 'file',
-    onChange: (info) => {
-      console.log('info!!', info);
+    accept: '.log',
+
+    beforeUpload: (file) => {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        setLogJsons(
+          e.target.result
+            .split('\n')
+            .map((line) => {
+              try {
+                return JSON.parse(line);
+              } catch (e) {
+                return null;
+              }
+            })
+            .filter(Boolean),
+        );
+      };
+
+      reader.readAsText(file);
+
+      // Prevent upload
+      return false;
     },
+
+    showUploadList: false,
   };
 
   return (
